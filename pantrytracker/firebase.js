cc31e61268d12c3b90f5148aaab2c39e7492347a
Vanohra <146138142+Vanohra@users.noreply.github.com>
2024-08-09 12:59:26 -0400

    added environmental variables

--- pantrytracker/firebase.js
+++ pantrytracker/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0lS3ivsPqpHnpM2a_nmi7tDpvr5m1vko",
-  authDomain: "pantry-tracker-6048d.firebaseapp.com",
-  projectId: "pantry-tracker-6048d",
-  storageBucket: "pantry-tracker-6048d.appspot.com",
-  messagingSenderId: "1055351345968",
-  appId: "1:1055351345968:web:ff932ceb4ab1890a242ef5",
-  measurementId: "G-TJG1XL431J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
